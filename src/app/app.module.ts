38f972edb455e860414a970f5b2feec6e4d02ed3
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-04-25 18:51:43 +0300

    update quotes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    QuotesFormComponent
+    QuotesFormComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
