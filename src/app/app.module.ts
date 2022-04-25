58eac37965295bfc6d5b7d8f914e9b3285da1593
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-04-25 15:39:44 +0300

    add quotes-form comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
