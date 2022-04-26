1c21ac0e799b330d00d901fa0d7b1eee72d600e8
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-04-26 22:30:36 +0300

    update test branch

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,15 +1,17 @@
 export class Quote {
-    id:number;
-    name:string;
-    title:string;
-    details:string;
-    author:string;
+    // id:number;
+    // name:string;
+    // title:string;
+    // details:string;
+    // author:string;
 
-    constructor(id:number, name:string, title:string, details:string, author:string){
-     this.id=id;
-     this.name=name;
-     this.title=title;
-     this.details=details;
-     this.author=author;   
+    showInfo:Boolean;
+    constructor(public id:number, public name:string, public title:string, public details:string, public author:string){
+     this.showInfo=false;   
+    //  this.id=id;
+    //  this.name=name;
+    //  this.title=title;
+    //  this.details=details;
+    //  this.author=author;   
     }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,13 +9,12 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
 
   quotes:Quote[]=[
-    {id:1, name:'Dansam1', title:'Coding', details:'If the code runs, do not touch it!', author:'Dansam Developers'},
-    {id:2, name:'Dansam2', title:'IDE', details:'A programmer must have a brain IDE and a computer IDE', author:'Dansam Developers'},
-    {id:3, name:'Dansam3', title:'Solitary', details:'A programmer has no life, though I have one', author:'Dansam Developers'},
-    {id:4, name:'Dansam4', title:'Debugging', details:'No debugging, no coding!', author:'Dansam Developers'},
-    {id:5, name:'Dansam5', title:'Just Give Up', details:'If things do not work at all, shut down machine, leave and never turn back!', author:'Dansam Developers'},
-    {id:6, name:'Dansam6', title:'Programmer Lovelive', details:'A male programmer needs a cool wife while a female programmer needs a co-wife!', author:'Dansam Developers'},
-
+    new Quote(1, 'Dansam1', 'Coding', 'If the code runs, do not touch it!', 'Dansam Developers'),
+    new Quote(2, 'Dansam2', 'IDE', 'A programmer must have a brain IDE and a computer IDE', 'Dansam Developers'),
+    new Quote(3, 'Dansam3', 'Solitary', 'A programmer has no life, though I have one', 'Dansam Developers'),
+    new Quote(4, 'Dansam4', 'Debugging', 'No debugging, no coding!', 'Dansam Developers'),
+    new Quote(5, 'Dansam5', 'Just Give Up', 'If things do not work at all, shut down machine, leave and never turn back!', 'Dansam Developers'),
+    new Quote(6, 'Dansam6', 'Programmer Lovelive', 'A male programmer needs a cool wife while a female programmer needs a co-wife!', 'Dansam Developers'),
 
   ];
   constructor() { }
